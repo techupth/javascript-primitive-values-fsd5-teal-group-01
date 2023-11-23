5d1a2d2b46bd8dab01ce4319927846507c939ffc
Krit <titatit.akkh@gmail.com>
2023-11-23 13:59:02 +0700

    feat: Finish EX

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let purchaseAmount = 27000;
+let discountCoupon = (25/100);
+let totalPrices = purchaseAmount - (purchaseAmount * discountCoupon);
+
+console.log(totalPrices)
